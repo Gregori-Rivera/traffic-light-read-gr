d9ee9d16a31096111b88dd81cfc670f5d8a59099
Gregori-Rivera <gregori.rivera@gmail.com>
2022-11-08 00:43:20 +0000

    agregar la barra del semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [light, setLight] = useState(undefined)
+	const handleOn = (changelight)=>{
+		if (light == changelight) {
+			setLight("apagado")
+		}
+		else {
+			setLight(changelight)
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+				<div className="stick"></div>
+				<div className="traffic-light">
+					<div className="centrador">
+						<div className={light == "red" ? "lights_red sombra_red" : "lights_red"} onClick={()=>{handleOn("red")}}></div>
+					</div >
+					<div className="centrador">
+						<div className={light == "yellow" ? "lights_yellow sombra_yellow" : "lights_yellow"} onClick={()=>{handleOn("yellow")}}></div>
+					</div>
+					<div className="centrador">
+					  <div className={light == "green" ? "lights_green sombra_green" : "lights_green"} onClick={()=>{handleOn("green")}}></div>
+					</div>
+				</div>
 		</div>
 	);
 };
